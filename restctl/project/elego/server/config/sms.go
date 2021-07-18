6787adf9d4c8e45f64fc7eec9b7296d12b8ddfc5
winlion <271151388@qq.com>
2021-07-18 20:59:27 +0800

    移除restctl

--- restctl/project/elego/server/config/sms.go
+++
@@ -1,6 +0,0 @@
-package config
-
-type Sms struct {
-	Tplcode  string `mapstructure:"tplcode" json:"tplcode" yaml:"tplcode" `
-	SignName string `mapstructure:"signname" json:"signname" yaml:"signname"`
-}
--- router.go
+++ router.go
@@ -1,7 +1,9 @@
 package restgo
 
 import (
+
 	"github.com/techidea8/restgo/middleware"
+	"html/template"
 	"log"
 	"net/http"
 	"strings"
@@ -16,6 +18,12 @@ var cors bool = false
 func EnableCors(){
 	cors = true
 }
+var debug bool = false
+func Debug(flag bool){
+	debug = flag
+}
+
+
 func DisableCors(){
 	cors = false
 }
@@ -129,4 +137,72 @@ func (p *GroupRouter) Get(act string, fun func(w http.ResponseWriter, req *http.
 //默认的路由规则
 func Router(pantern string, fun func(w http.ResponseWriter, req *http.Request)) (p *GroupRouter) {
 	return Module("").Router(pantern, fun)
+}
+
+//文件服务器支持
+//Aliase("/assets/","./resource/assets/")
+//外部请求  http://[ip]/assets/img/test.png,系统将
+func Aliase(patern string,dirpath string) {
+	http.Handle(patern, http.StripPrefix(patern, http.FileServer(http.Dir(dirpath))))
+}
+
+//文件服务器支持
+//Root("/assets/","./resource/assets/")
+//外部请求  http://[ip]/assets/img/test.png,系统将
+func Root(patern string,dirpath string) {
+	http.Handle(patern, http.StripPrefix("", http.FileServer(http.Dir(dirpath))))
+}
+
+//自定义函数支持
+var restFuncMap template.FuncMap = make(template.FuncMap)
+type Fun  func(arg ...interface{})interface{}
+func RegisterFuncMap(fname string,fun Fun) {
+	restFuncMap[fname] = fun
+}
+var globalTemplete *template.Template
+//全局函数
+var globalData map[string]interface{} = make(map[string]interface{})
+func AddDataToTemplete(key string,data interface{}){
+	globalData[key] = data
+}
+
+//模板文件批量注册
+//registertempletes("/","view/**/*.html","index/index.html")\n
+// @title    模板解析函数\n
+// @description   批量解析模板文件并自动映射成相应url\n
+// @auth      winlion             时间（2019/6/18   10:57 ）\n
+// @param     prefix        string         "页面访问前奏,类似spring boot中的context"\n
+// @param     patern        string         "模板文件路径格式,如view/**/*.html"\n
+// @param     indextplname        string         "首页模板文件路径"\n
+// @return    无\n
+func RegisterTempletes(prefix string,patern string,indextplname string) {
+
+		_globalTemplete, err := template.ParseGlob(patern)
+		if err!=nil{
+			//fmt.Println(err.Error())
+			log.Default().Printf("register tpl  [%s] error:%s",patern, err.Error())
+			return
+		}
+		globalTemplete= _globalTemplete
+		for _,v := range globalTemplete.Templates(){
+
+			tplname := v.Name()
+				//http.Handle()
+				if debug{
+					log.Default().Printf("map TPL [%s]=>%s\n",tplname,prefix+tplname)
+				}
+                contextpath := prefix+tplname
+                if tplname==indextplname{
+                	contextpath = prefix
+				}
+				http.HandleFunc(contextpath, func(w http.ResponseWriter, req *http.Request) {
+					//如果再次
+					if debug{
+						globalTemplete, _ = template.ParseGlob(patern)
+					}
+					log.Default().Printf("prefix+v.Name()=>%s",prefix+v.Name())
+					globalTemplete.Funcs(restFuncMap).ExecuteTemplate(w,tplname,globalData)
+				})
+			}
+
 }
