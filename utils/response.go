0f48f21742f46c28cb27e15deac622d0fcf42bc0
techidea8 <betaniao@qq.com>
2023-03-16 22:59:13 +0800

    A

--- utils/response.go
+++ utils/response.go
@@ -7,9 +7,10 @@ import (
 
 type Result struct {
 	Code  int         `json:"code"`
-	Data  interface{} `json:"data,omitempty"`
-	Msg   string      `json:"msg,omitempty"`
-	Total int64       `json:"total,omitempty"`
+	Data  interface{} `json:"data"`
+	Rows  interface{} `json:"rows"`
+	Msg   string      `json:"msg"`
+	Total int64       `json:"total"`
 }
 
 func (r Result) ResponseJson(w http.ResponseWriter) {
@@ -19,32 +20,44 @@ func (r Result) ResponseJson(w http.ResponseWriter) {
 func ResultRows(data interface{}, total int64) Result {
 	return Result{
 		Code:  http.StatusOK,
-		Data:  data,
+		Rows:  data,
 		Total: total,
+		Data:  map[string]interface{}{},
+		Msg:   "",
 	}
 }
 func ResultOkMsg(msg string) Result {
 
 	return Result{
-		Code: http.StatusOK,
-		Msg:  msg,
+		Code:  http.StatusOK,
+		Msg:   msg,
+		Data:  map[string]interface{}{},
+		Rows:  make([]string, 0),
+		Total: 0,
 	}
 }
 func ResultOk(data ...interface{}) Result {
 	if len(data) == 0 {
 		return Result{
-			Code: http.StatusOK,
+			Code:  http.StatusOK,
+			Data:  map[string]interface{}{},
+			Rows:  make([]string, 0),
+			Total: 0,
 		}
 	} else if len(data) == 1 {
 		return Result{
-			Code: http.StatusOK,
-			Data: data[0],
+			Code:  http.StatusOK,
+			Data:  data[0],
+			Rows:  make([]string, 0),
+			Total: 0,
 		}
 	} else {
 		return Result{
-			Code: http.StatusOK,
-			Data: data[0],
-			Msg:  data[0].(string),
+			Code:  http.StatusOK,
+			Data:  data[0],
+			Msg:   data[1].(string),
+			Rows:  make([]string, 0),
+			Total: 0,
 		}
 	}
 }
@@ -56,7 +69,10 @@ func ResultError(msg any) Result {
 		str = str1.Error()
 	}
 	return Result{
-		Code: http.StatusNotFound,
-		Msg:  str,
+		Code:  http.StatusNotFound,
+		Msg:   str,
+		Data:  map[string]interface{}{},
+		Rows:  make([]string, 0),
+		Total: 0,
 	}
 }
