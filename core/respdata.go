9c344936d16cfff65728b50ec1e696a771cc7bc5
turingdance <271151388@qq.com>
2023-12-17 11:21:21 +0800

    suport HttpStatus

--- core/respdata.go
+++ core/respdata.go
@@ -7,15 +7,18 @@ import (
 )
 
 type RespData struct {
-	Code  int         `json:"code"`
-	Rows  interface{} `json:"rows,omitempty"`
-	Data  interface{} `json:"data,omitempty"`
-	Msg   string      `json:"msg,omitempty"`
-	Total interface{} `json:"total,omitempty"`
+	Code       int         `json:"code"`
+	Rows       interface{} `json:"rows,omitempty"`
+	Data       interface{} `json:"data,omitempty"`
+	Msg        string      `json:"msg,omitempty"`
+	Total      interface{} `json:"total,omitempty"`
+	HttpStatus int         `json:"-"`
 }
 
 func NewRespData() *RespData {
-	return &RespData{}
+	return &RespData{
+		HttpStatus: http.StatusOK,
+	}
 }
 
 // 返回msg
@@ -24,6 +27,7 @@ func (r *RespData) Ok(msgs ...string) *RespData {
 		r.Msg = strings.Join(msgs, ",")
 	}
 	r.Code = http.StatusOK
+	r.HttpStatus = http.StatusOK
 	return r
 }
 
@@ -31,6 +35,7 @@ func (r *RespData) Ok(msgs ...string) *RespData {
 func (r *RespData) Fail(msg string) *RespData {
 	r.Msg = msg
 	r.Code = http.StatusNotFound
+	r.HttpStatus = http.StatusOK
 	return r
 }
 
@@ -59,9 +64,15 @@ func (r *RespData) WithTotal(total interface{}) *RespData {
 }
 
 // 返回msg
+func (r *RespData) WithHttpStatus(code int) *RespData {
+	r.HttpStatus = code
+	return r
+}
+
+// 返回msg
 func (r *RespData) Json(w http.ResponseWriter) {
 	header := w.Header()
 	header.Set("Content-Type", "application/json;charset=utf-8")
-	w.WriteHeader(r.Code)
+	w.WriteHeader(r.HttpStatus)
 	json.NewEncoder(w).Encode(*r)
 }
